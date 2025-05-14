7da05b728e382cfbc047c47c55a831b288809465
greice-mrq <greicemarques332@gmail.com>
2025-05-14 00:02:37 +0000

    salvo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { mainModule } from "process";
+import { sequelize } from "./config/database"
+
 
 const app = express();
 const port = 3000;
@@ -7,4 +10,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+
+main()
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({force: true}).then(() =>{
+      console.log("Database is synced");
+      app.listen(port, () =>
+        console.log('listening at http://localhost:${port}'),
+    );
+    });
+
+    // criar usuario
+    const user = await User.create({ name: 'Alice', email:'alice@email.com'});
+    console.log("Usuario criado:", user.toJSON());
+  }
+}
+ 
